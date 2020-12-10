e574e7b713de0109632f753a2ca5629345381f20
vishwasredefined <vishwasredefined@gmail.com>
2020-12-10 12:24:31 +0530

    Faveo Docker V2 first commit

---
+++ Readme.md
@@ -0,0 +1,36 @@
+Only the following Editions of Faveo-Helpdesk are supported Freelancer, Paid and Enterprise.
+
+Complete the below steps to get the Faveo-Helpdesk Containers up.
+
+Provide executable permission to faveo-run.sh
+
+```sh
+chmod +x faveo-run.sh
+```
+
+Prerequisites to run the script:
+
+1. A valid domain name fully propagated to you Server's IP.
+2. Sudo Privilege.
+3. Faveo license and Order number.
+4. Unreserved ports 80 and 443.(If it is reserved feel free to edit and change the ports of your choice in docker-copompose.yml)
+5. Operating Systems Centos 7,8 or above and Ubuntu 16,18,20.
+
+Run the script "faveo-run.sh" with sudo privilege by passing the necessary arguments.
+
+Note: You should have a Valid domain name pointing to your public IP. Since this domainname is used to obtain SSL Certificates from Let's Encrypt CA and the Email is used for the same process.The license code and Order Number can be obtained from your Faveo Helpdesk Billing portal, make sure not to include the '#' character from Order Number. 
+
+Usage:
+```sh
+	sudo ./faveo-run.sh -domainname <your domainname> -email <example@email.com> -license <faveo license code> -orderno <faveo order number>
+```
+Example: It should look something like this.
+```sh
+      sudo ./faveo-run.sh -domainname berserker.tk -email berserkertest@gmail.com -license 5H876HHDGDIBK0000 -orderno 81230569
+```
+After the docker installation completed you will be prompted with Database Credentials please copy and save them somewhere safe.
+
+Visit https://<yourdomainname> complete the readiness probe, input the Database Details when prompted and complete the installation.
+
+There is one final step needs to be done in order complete the installation. You have to edit the .env file which is generated in the Faveo root directory after completing the installation in browser. Open terminal and navigate to the faveo-docker directory here you will find the directory "faveo" which is downloaded while running the script this directory contains all the Helpdesk codebase, inside it you need to edit the ".env" file and update the "Redis Host" value to "redis" by default it will be pointing to loopback address "127.0.0.1" here redis is the DNS name of redis container which will be resolved by the docker daemon.
+
---
+++ faveo-run.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+if [[ $# -lt 8 ]]; then
+    echo "Please run the script by passing all the required arguments."
+    exit 1;
+fi
+
+echo "Checking Prerequisites....."
+setenforce 0
+apt update; apt install unzip curl -y || yum install unzip curl -y
+
+if [[ $? -eq 0 ]]; then
+    echo "Prerequisites check completed."
+else
+    echo "Check failed please make sure to execute the script as sudo user and also check your Internet connectivity."
+    exit 1;
+fi
+CUR_DIR=$(pwd)
+host_root_dir="faveo"
+# Evaluate the arguments
+while test $# -gt 0; do
+        case "$1" in
+                -domainname)
+                    shift
+                    domainname=$1
+                    shift
+                    ;;
+                -email)
+                    shift
+                    email=$1
+                    shift
+                    ;;
+                -license)
+                    shift
+                    license=$1
+                    shift
+                    ;;
+                -orderno)
+                    shift
+                    orderno=$1
+                    shift
+                    ;;
+                *)
+                echo "$1 is not a recognized flag!"
+                exit 1;
+                ;;
+        esac
+done
+echo -e "\n";
+echo -e "Confirm the Entered Helpdesk details:\n";
+echo -e "-------------------------------------\n"
+
+echo "Domain Name : $domainname";
+echo "Email: $email";
+echo "License Code: $license";
+echo "Order Number: $orderno";
+echo -e "\n";
+read -p "Continue (y/n)?" REPLY
+
+if [[ ! $REPLY =~ ^(yes|y|Yes|YES|Y) ]]; then
+        exit 1;
+fi;
+
+if [ ! -d $CUR_DIR/certbot/html ]; then
+    mkdir -p $CUR_DIR/certbot/html
+elif [ ! -e $CUR_DIR/certbot/html ]; then
+    exit 0;
+fi;
+
+echo "<h1>Obtain SSL Certs</h1>" > $CUR_DIR/certbot/html/index.html
+
+echo -e "Initializing Temporary Apache container to obtain SSL Certificates..."
+
+docker run -dti -p 80:80 -v $CUR_DIR/certbot/html:/usr/local/apache2/htdocs --name apache-cert httpd:2.4.33-alpine
+
+if [[ $? -eq 0 ]]; then
+    echo "Initializing Certbot Container to obtain SSL Certificates for $domainname"
+    docker run -ti --rm -v $CUR_DIR/certbot/letsencrypt/etc/letsencrypt:/etc/letsencrypt -v $CUR_DIR/certbot/html:/data/letsencrypt --name certbot certbot/certbot certonly --webroot --email $email  --agree-tos --non-interactive  --no-eff-email --webroot-path=/data/letsencrypt -d $domainname
+else
+    echo "Temporary Container Failed to Initialise exiting..."
+    exit 1;
+fi;
+
+docker rm -f apache-cert
+
+chown -R $USER:$USER $CUR_DIR/certbot
+
+if [[ $? -eq 0 ]]; then
+    echo "SSL Certificates for $domainname obtained Successfully."
+else
+    echo "Permission Issue."
+    exit 1;
+fi;
+
+echo "Downloading Faveo Helpdesk"
+
+curl https://billing.faveohelpdesk.com/download/faveo\?order_number\=$orderno\&serial_key\=$license --output faveo.zip
+
+
+if [[ $? -eq 0 ]]; then
+    echo "Download Successfull";
+else
+    echo "Download Failed. Please check the order number, serial number of Helpdesk entered and your Internet connectivity."
+    exit 1;
+fi;
+
+if [ ! -d $CUR_DIR/$host_root_dir ]; then
+    unzip faveo.zip -d $host_root_dir
+else
+    rm -rf $CUR_DIR/$host_root_dir
+    unzip faveo.zip -d $host_root_dir
+fi
+
+if [ $? -eq 0 ]; then
+    chown -R 33:33 $host_root_dir
+    find $host_root_dir -type d -exec chmod 755 {} \;
+    find $host_root_dir -type f -exec chmod 644 {} \;
+else
+    echo "Extract failure."
+fi
+
+db_root_pw=$(openssl rand -base64 12)
+db_name=faveo
+db_user=faveo
+db_user_pw=$(openssl rand -base64 12)
+
+
+if [[ $? -eq 0 ]]; then
+    rm -f .env
+    cp example.env .env
+    sed -i 's:MYSQL_ROOT_PASSWORD=:&'$db_root_pw':' .env
+    sed -i 's/MYSQL_DATABASE=/&'$db_name'/' .env
+    sed -i 's/MYSQL_USER=/&'$db_user'/' .env
+    sed -i 's:MYSQL_PASSWORD=:&'$db_user_pw':' .env
+    sed -i 's/DOMAINNAME=/&'$domainname'/' .env
+    sed -i '/ServerName/c\    ServerName '$domainname'' ./apache/000-default.conf
+    sed -i 's/HOST_ROOT_DIR=/&'$host_root_dir'/' .env
+    sed -i 's:CUR_DIR=:&'$PWD':' .env
+else
+    echo "Database Password Generation Failed"
+fi
+
+
+if [[ $? -eq 0 ]]; then
+    docker volume create --name ${domainname}-faveoDB
+fi
+
+if [[ $? -eq 0 ]]; then
+    docker network create ${domainname}-frontend
+    docker network create ${domainname}-backend
+
+else
+     echo "Docker volume creation failed."
+     exit 1;
+fi
+
+
+
+if [[ $? -eq 0 ]]; then
+    docker-compose up -d
+else
+    echo "Docker Network  Creation Failed"
+    exit 1;
+fi
+
+if [[ $? -eq 0 ]]; then
+    echo -e "\n"
+    echo "#########################################################################"
+    echo -e "\n"
+    echo "Faveo Docker installed successfully. Visit https://$domainname from your browser."
+    echo "Please save the following credentials."
+    echo "Database Hostname: mariadb"
+    echo "Mysql Database root password: $db_root_pw"
+    echo "Faveo Helpdesk name: $db_name"
+    echo "Faveo Helpdesk DB User: $db_user"
+    echo "Faveo Helpdesk DB Password: $db_user_pw"
+    echo -e "\n"
+    echo "#########################################################################"
+else
+    echo "Script Failed unknown error."
+    exit 1;
+fi
